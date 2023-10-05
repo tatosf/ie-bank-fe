0c895e2456ed06430cb726b97b1a03773a51aeea
tatosf <santiagofischel1@gmail.com>
2023-10-05 14:13:13 +0200

    basic home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home-page">
+      <h1>Welcome to the IE Bank!</h1>
+      <button @click="goToAccounts">Go to Accounts</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      goToAccounts() {
+        this.$router.push('/accounts');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .home-page {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
+import HomePage from '../components/HomePage.vue' // Import the HomePage component
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',  // Add a path for the root URL
+    name: 'Home', // Name it 'Home'
+    component: HomePage  // And use HomePage as the component
+  },
   {
     path: '/skull',
     name: 'Skull',
